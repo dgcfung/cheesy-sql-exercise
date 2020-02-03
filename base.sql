00e55b117e61df41047b1b8a8ccdda85db980b1b
Steve VanWoerkom <steven.vanwoerkom@generalassemb.ly>
2020-02-03 09:49:15 -0500

    Update base.sql

--- base.sql
+++ base.sql
@@ -1,1 +1,16 @@
+DROP DATABASE IF EXISTS cheeses_db;
+CREATE DATABASE cheeses_db;
+
+\c cheeses_db;
+
+DROP TABLE IF EXISTS cheese;
+
+CREATE TABLE cheese (
+  cheese_id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  color VARCHAR(255),
+  origin VARCHAR(255),
+  stink_level INTEGER
+);
+
 -- add your seed data here!!!! 
